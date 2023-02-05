4d5e46fce08ea9260fa0560026d026a7664b0ef2
JuriiGorunov <pro100krash63rus@rambler.ru>
2023-02-05 15:22:32 +0400

    Отцентрировали bots

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ $(document).ready(function () {
     infinite: true,
     slidesToShow: 5,
     slidesToScroll: 1,
+    dots: true,
+    
     prevArrow: "<button type='button' class='btn-slick-prev btn btn-slider'><img src='images/icons/left-arrow.svg' alt='left-arrow'></button>",
     nextArrow: "<button type='button' class='btn-slick-next btn btn-slider'><img src='images/icons/right-arrow.svg' alt='right-arrow'></button>"
   });
